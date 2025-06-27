7d38a31e06eeba68561f80f3c090b08faf727742
EmersonAbel23 <abel.yauri1901@gmail.com>
2025-06-26 21:08:04 -0500

    prueba principal

---
+++ app.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector("#icono-menu");
+let navegacion = document.querySelector(".navegacion");
+
+
+
+menu.addEventListener("click", function(){
+	navegacion.classList.toggle("active");
+});
+
+window.onscroll = () =>{
+	navegacion.classList.remove("active")
+}
+
+
+
+
